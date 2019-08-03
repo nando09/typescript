a59c01390861f900188e64cc0ba8b1fa86a72861
nando09 <nando.online@live.com>
2019-08-03 11:57:45 -0300

    Comando typescript

---
+++ typescript/app.ts
@@ -0,0 +1,7 @@
+class Carro(){
+	private modelo: String
+	private numeroPorta: Number
+	private velocidade: Number
+
+	constructor	
+}
---
+++ typescript/dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+var Carro = /** @class */ (function () {
+    function Carro() {
+    }
+    return Carro;
+}());
+(function () {
+    modelo: String;
+    numeroPorta: Number;
+    velocidade: Number;
+    constructor;
+});
